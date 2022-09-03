dcb90b0d4fa08913000c228ccaa049a389c42199
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-09-03 17:28:28 -0500

    :art: code

--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 const palabra = document.getElementById('add-word-textarea');
 
 function guardarPalabra() {
-
     let palabras = JSON.parse(localStorage.getItem('palabras'));
-
     if (!palabras) {
         palabras = [];
-    } 
+    }
     if (palabra.value.length > 0) {
         palabras.push(palabra.value);
-        palabra.value='';
+        palabra.value = '';
         localStorage.setItem('palabras', JSON.stringify(palabras))
         window.location.href = 'ahorcado.html';
     } else {
